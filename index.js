82fd9c6cf7b3de19fb6a5f57bc6d4241129edde6
tajveez <tajveez786@gmail.com>
2021-10-31 22:08:51 +0500

    creating router

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ success: true });
-});
+app.use("/api", require("./routes"));
 
 // Enable
 app.use(cors());
