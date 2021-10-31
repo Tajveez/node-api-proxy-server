6c37112ce8bc2adfd95ccc1701fc8e504fc02c69
tajveez <tajveez786@gmail.com>
2021-10-31 22:25:41 +0500

    logging in dev mode

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,11 @@ router.get("/", async (req, res) => {
     const apiRes = await needle("get", `${API_BASE_URL}?${params}`);
     const data = apiRes.body;
 
+    if (process.env.NODE_ENV !== "production") {
+      console.log(`REQUEST: ${API_BASE_URL}?${params}`);
+      console.log(`RESPONSE: ${JSON.stringify(data)}`);
+    }
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error });
