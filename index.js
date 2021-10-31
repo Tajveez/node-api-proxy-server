d32ad809b64da7d1ebb724ac44c299b55252cf11
tajveez <tajveez786@gmail.com>
2021-10-31 22:54:33 +0500

    Cache and Limiter implemented

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate Limiting
+const limiter = rateLimiter({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  message: "Api rate limit exhausted",
+});
+
+// Setting the limit for API call
+app.use(limiter);
+app.set("trust proxy", 1);
+
+// Setting the static folder
+app.use(express.static("public"));
 
 app.use("/api", require("./routes"));
 
